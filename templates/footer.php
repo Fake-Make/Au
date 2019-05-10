38c50ac6cea5409034929bf2b3b88bae825950dc
Dmitry Nevada <vegnerdima@yandex.ru>
2019-05-10 14:39:31 +0700

    Footer now always at the bottom of page;

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,13 @@
-</main>
-	<footer class="flex-column page-footer">
-		<a class="image-logo" href="index.php"><img src="img/logo.png" alt="logo" height="30px"></a>
-		<div class="flex-column">
-		<p>Используя сайт, вы принимаете соглашение</p>
-			<p>Copyright © интернет-аукцион au.ru</p>
-			<p>2019-2019 Все права защищены</p>
+			</main>
 		</div>
-	</footer>
+		<footer class="flex-column page-footer">
+			<a class="image-logo" href="index.php"><img src="img/logo.png" alt="logo" height="30px"></a>
+			<div class="flex-column">
+			<p>Используя сайт, вы принимаете соглашение</p>
+				<p>Copyright © интернет-аукцион au.ru</p>
+				<p>2019-2019 Все права защищены</p>
+			</div>
+		</footer>
+	</div>
 </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -7,19 +7,21 @@
 	<link rel="shortcut icon" href="img/favicon.png" type="image/png">
 	<title>Au - Площадка Интернет-Аукционов</title>
 </head>
-<body class="flex-column">
-	<header class="flex-column page-header">
-		<nav class="flex-column page-nav">
-			<ul class="flex-row wrapper">
-				<li><a class="image-logo" href="index.php"><img src="img/logo.png" alt="Аукционы" height="30px"></a></li>
-				<li><a href="login.php">Войти</a> или <a href="registration">зарегистрироваться</a> | <a class="flex-row" href="personal.php">User Name<img class="user-image" src="img/box.png" alt="Фото пользователя"></a></li>
-			</ul>
-		</nav>
-		<?if(false !== strpos($_SERVER["REQUEST_URI"], "index")):?>
-			<form action="index.php" class="flex-row wrapper">
-				<input class="input-box search-input" type="text" placeholder="Название лота">
-				<input class="button search-submit" type="submit" value="Поиск">
-			</form>
-		<?endif?>
-	</header>
-	<main class="flex-column wrapper">
+<body>
+	<div class="height-wrapper">
+		<div class="content-wrapper">
+			<header class="flex-column page-header">
+				<nav class="flex-column page-nav">
+					<ul class="flex-row wrapper">
+						<li><a class="image-logo" href="index.php"><img src="img/logo.png" alt="Аукционы" height="30px"></a></li>
+						<li><a href="login.php">Войти</a> или <a href="registration">зарегистрироваться</a> | <a class="flex-row" href="personal.php">User Name<img class="user-image" src="img/box.png" alt="Фото пользователя"></a></li>
+					</ul>
+				</nav>
+				<?if(false !== strpos($_SERVER["REQUEST_URI"], "index")):?>
+					<form action="index.php" class="flex-row wrapper">
+						<input class="input-box search-input" type="text" placeholder="Название лота">
+						<input class="button search-submit" type="submit" value="Поиск">
+					</form>
+				<?endif?>
+			</header>
+			<main class="flex-column wrapper">
