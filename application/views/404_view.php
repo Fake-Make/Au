1ea5a203777cd5b92bc282bdb56d2af5d8d4d16d
Dmitry Nevada <vegnerdima@yandex.ru>
2019-06-07 14:00:18 +0700

    404-page fix: from picture to text;

--- application/views/404_view.php
+++ application/views/404_view.php
@@ -1,4 +1,4 @@
-<h1 class="invisible">Ошибка 404</h1>
-<p>
-<img src="img/404.png">
+<h1 style="color: #09f; font-weight: bold; font-size: 42px">Ошибка 404!</h1>
+<p style="color: #09f; text-align: center; font-size: 24px;">
+Запрашиваемая страница не найдена :(
 </p>
